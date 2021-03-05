d56933b6206e494278a48cc6929d24c9d7ea8b9f
RobotMermaid <ondinerangel@gmail.com>
2021-03-05 10:01:37 -0800

    deploy?

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const port = 3000
 
 const Webflow = require('webflow-api')
 require('isomorphic-fetch');
+
 const api = new Webflow({ token: 'eb26dc01a9366ade490b6c589b742109484afdee5262ffa3c8970b5694430096' })
 let siteId = '600b1f4c2115e03205c5abf6';
 let collectionId = '600f09e9ae7a0d0dbb875d89';
 let ids = {};
 
 app.get('/hello', (req, res) => {
-    res.json({key:"value"});
+    // res.json({key:"value"});
     fetch('https://spacelaunchnow.me/api/3.5.0/config/spacecraft/?limit=200')
         .then(function(response) {
             if (response.status >= 400) {
