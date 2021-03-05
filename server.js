97e657b6746c29b75a4719093024c5490c69b790
RobotMermaid <ondinerangel@gmail.com>
2021-03-05 10:27:39 -0800

    port fix 2

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at port:${PORT}`)
 })
