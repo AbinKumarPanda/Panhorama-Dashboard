fc9bd41a9fe102de9514c1896561400888d6b9f9
RobotMermaid <ondinerangel@gmail.com>
2021-03-09 13:01:24 -0800

    removes dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+// require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 
 const Webflow = require('webflow-api')
